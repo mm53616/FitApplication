2bf4d521596993d51877ca17f64b322491bafe12
mm53616 <m.manthey53616@gmail.com>
2018-05-02 16:08:20 +0200

    Fixing exercises activity and creating the "exercise!" activity.

--- app/src/main/java/com/example/ania/fitapplication/Exercises.java
+++ app/src/main/java/com/example/ania/fitapplication/Exercises.java
@@ -1,7 +1,9 @@
 package com.example.ania.fitapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Exercises extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class Exercises extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercises);
     }
+
+    public void Go(View view){
+        Intent i = new Intent(this, Exercise.class);
+        startActivity(i);
+    }
+
+
 }
--- app/src/main/java/com/example/ania/fitapplication/MainActivity.java
+++ app/src/main/java/com/example/ania/fitapplication/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     public void Exit(View view){
         finish();
     }
